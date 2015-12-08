f618e62bf033a9a37ed8010941c6ac5af9775698
Thibaud Arguillere <targuillere@nuxeo.com>
2015-12-08 10:32:45 -0500

    Fix pb with date picker

--- pensco-utils-web/src/main/resources/web/nuxeo.war/pensco/resources/javascript/demo.js
+++ pensco-utils-web/src/main/resources/web/nuxeo.war/pensco/resources/javascript/demo.js
@@ -1,79 +1,70 @@
 var API = (function(API) {
 
-	var nuxeoClient;
+    var nuxeoClient;
 
-	API.config = function () {
-	  //Instantiate Nuxeo Client
-	  nuxeoClient = new nuxeo.Client({
-	  	timeout: 3000}
-	  );
-	  // Client schema and timeout configuration
-	  nuxeoClient.schema("*");
-	};
+    API.config = function() {
+        // Instantiate Nuxeo Client
+        nuxeoClient = new nuxeo.Client({
+            timeout : 3000
+        });
+        // Client schema and timeout configuration
+        nuxeoClient.schema("*");
+    };
 
-	API.search = function(data) {
-	  nuxeoClient.repositoryName('default').
-		operation('API-GenerateMergedCustomerStatements').
-		params(data).
-		execute(callbackGeneratePDF);
-	  $("#inputForm").attr("class", "ui loading form segment");
-	};
+    API.search = function(data) {
+        nuxeoClient.repositoryName('default').operation('API-GenerateMergedCustomerStatements').params(data).execute(
+                callbackGeneratePDF);
+        $("#inputForm").attr("class", "ui loading form segment");
+    };
 
-	////////////////////////////// CALLBACK FUNCTIONS
+    // //////////////////////////// CALLBACK FUNCTIONS
 
-	function callbackGeneratePDF(error, result) {
+    function callbackGeneratePDF(error, result) {
 
-	  $("#inputForm").attr("class", "ui form segment");
+        $("#inputForm").attr("class", "ui form segment");
 
-	  if (error) {
-		throwError("Cannot run generate pdf -> " + error);
-		throw error;
-	  }
-	  console.log('Download details are ' + result);
-	  var url = result.properties["file:content"].data;
-	  download(url);
-	};
+        if (error) {
+            throwError("Cannot run generate pdf -> " + error);
+            throw error;
+        }
+        console.log('Download details are ' + result);
+        var url = result.properties["file:content"].data;
+        download(url);
+    }
+    ;
 
-	return API;
+    return API;
 
 })({});
 
+// //////////////////////////// INIT
+$(document).ready(function() {
 
-////////////////////////////// INIT
-$(document).ready(function () {
-	$('#fromDate').datetimepicker({
-	  timepicker:false,
-	  format:'d/m/Y'});
-	$('#toDate').datetimepicker({
-      timepicker:false,
-      format:'d/m/Y'});
+    // The "name" attribute is the same as the name of the variables expected by the automation chain in nuxeo
+    $('#submitButton').click(function() {
+        var data = new Object();
+        $("#inputForm :input").each(function() {
+            var name = $(this).attr('name');
+            var input = $(this).val();
+            data[name] = input;
+        });
+        API.search(data);
+    });
 
-	$('#submitButton').click(function () {
-		var data = new Object();
-			$("#inputForm :input").each(function () {
-			var name = $(this).attr('name');
-			var input = $(this).val();
-			data[name] = input;
-		});
-		API.search(data);
-	});
+    $('#clearButton').click(function() {
+        $('#inputForm').form('clear')
+    });
 
-	$('#clearButton').click(function () {
-		$('#inputForm').form('clear')
-	});
-
-	API.config();
+    API.config();
 });
-
 
 // Show modal for error
 function throwError(error) {
-  $('#errorValue').append('No Statement found');
-  $('#modalError').modal('show');
+    $('#errorValue').append('No Statement found');
+    $('#modalError').modal('show');
 };
 
 //Download file
-function download(file)
-{
- window.location=file;
+function download(file) {
+    window.location = file;
 };
